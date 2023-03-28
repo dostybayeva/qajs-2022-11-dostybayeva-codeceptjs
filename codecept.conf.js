6e5919b8d655b0a794b8b76faee985aa4f23b9df
dostybayeva <dostybayeva@kolesa.kz>
2023-03-28 23:21:48 +0600

    подключаю репортинг

--- codecept.conf.js
+++ codecept.conf.js
@@ -19,17 +19,18 @@ exports.config = {
   },
   include: {
     I: './steps_file.js',
-    loginPage: "./framework/pages/Login.js",
-    addEmployeePage: "./framework/pages/AddEmployee.js",
-    employeeListPage: "./framework/pages/EmployeeList.js",
-    employeeDetailPage: "./framework/pages/EmployeeDetail.js",
-    employeeElements: "./framework/elements/Employee.js"
+    loginPage: './framework/pages/Login.js',
+    addEmployeePage: './framework/pages/AddEmployee.js',
+    employeeListPage: './framework/pages/EmployeeList.js',
+    employeeDetailPage: './framework/pages/EmployeeDetail.js',
+    employeeElements: './framework/elements/Employee.js'
   },
   name: 'qajs-2022-11-dostybayeva-codeceptjs',
   plugins: {
     allure: {
       enabled: true,
-      require: '@codeceptjs/allure-legacy'
+      require: '@codeceptjs/allure-legacy',
+      outputDir: 'allure-results'
     }
   }
 }
